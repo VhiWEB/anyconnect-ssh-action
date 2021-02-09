64a8c88df18ade01891775bcf9d9cad162d3d19f
Trias Nur Rahman <contact@triasrahman.com>
2021-02-10 00:12:43 +0700

    Update entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,5 @@
 # Connect to VPN
 ( echo "yes"; echo $INPUT_VPN_PASSWORD ) | openconnect $INPUT_VPN_HOST --user=$INPUT_VPN_USER --authgroup=$INPUT_VPN_GROUP
 
-# Prepare Private Key
-echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
-
 # Execute SSH Command
-ssh -o 'StrictHostKeyChecking no' $INPUT_SSH_USER@$INPUT_SSH_HOST -p $INPUT_SSH_PORT $INPUT_SSH_COMMAND
+echo $SSH_PRIVATE_KEY | ssh -o 'StrictHostKeyChecking no' -i /dev/stdin -p $INPUT_SSH_PORT $INPUT_SSH_USER@$INPUT_SSH_HOST $INPUT_SSH_COMMAND
