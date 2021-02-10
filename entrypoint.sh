651655f5751300a053249e8306012a6ab9601300
Trias Nur Rahman <contact@triasrahman.com>
2021-02-10 11:20:23 +0700

    Add private key mechanism

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 # Connect to VPN
 ( echo "yes"; echo $INPUT_VPN_PASSWORD ) | openconnect $INPUT_VPN_HOST --user=$INPUT_VPN_USER --authgroup=$INPUT_VPN_GROUP
 
+# Prepare private-key
+echo $SSH_PRIVATE_KEY > ssh-private-key
+
 # Execute SSH Command
-echo $SSH_PRIVATE_KEY | ssh -o 'StrictHostKeyChecking no' -i /dev/stdin -p $INPUT_SSH_PORT $INPUT_SSH_USER@$INPUT_SSH_HOST $INPUT_SSH_COMMAND
+ssh -o 'StrictHostKeyChecking no' -i ssh-private-key -p $INPUT_SSH_PORT $INPUT_SSH_USER@$INPUT_SSH_HOST $INPUT_SSH_COMMAND
